c7b11788b28be5ee03c1a814a61482810728fb7a
nhtoshiaki <nh_toshi.94@hotmail.com>
2018-11-01 15:41:17 -0300

    Enabling bspwm module on polybar.

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -8,6 +8,8 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 export MONITOR1=$(xrandr | awk '{ if ($2 == "connected" && $3 == "primary") { print $1 } }')
 export MONITOR2=$(xrandr | awk '{ if ($2 == "connected" && $3 ~ /^[1-9]/) { print $1 } }' | head -n 1)
+echo "Monitor1: $MONITOR1"
+echo "Monitor2: $MONITOR2"
 
 # Launch bars
 if [[ $MONITOR1 ]]; then
@@ -17,7 +19,7 @@ fi
 
 if [[ $MONITOR2 ]]; then
     #polybar -r top2 &
-    polybar -r bottom2 &
+    env MONITOR2=$MONITOR2 polybar -r bottom2 &
 fi
 
 echo "Bars launched..."
