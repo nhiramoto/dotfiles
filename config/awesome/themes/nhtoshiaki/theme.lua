5ee1bde41a87617139cd9baebdc38b473d00677a
nhtoshiaki <nicolau.hiramoto@gmail.com>
2019-02-21 13:23:52 -0300

    Changing awesomewm theme.

--- config/awesome/themes/nhtoshiaki/theme.lua
+++ config/awesome/themes/nhtoshiaki/theme.lua
@@ -1,8 +1,8 @@
----------------------------------
---  "nhtoshiaki" awesome theme --
---    Based on "zenburn"       --
---    By nhtoshiaki            --
----------------------------------
+----------------------------------------------------------
+--  "nhtoshiaki" awesome theme                          --
+--    Based on "apprentice" color theme from vim        --
+--    By nhtoshiaki                                     --
+----------------------------------------------------------
 
 local gears = require("gears")
 local theme_assets = require("beautiful.theme_assets")
@@ -14,27 +14,29 @@ local theme = {}
 
 -- {{{ Styles
 
+theme.panel           = "png:" .. thistheme_path .. "/panel.png"
+
 -- {{{ Fonts
-theme.font      = "Exo 2 Semibold 9"
-theme.font_small = "Exo 2 Semibold 8"
-theme.font_big = "Exo 2 Semibold 10"
+theme.font      = "Hurmit Nerd Font Mono Semibold 8"
+theme.font_small = "Hurmit Nerd Font Mono Semibold 7"
+theme.font_big = "Hurmit Nerd Font Mono Semibold 9"
 -- }}}
 
 -- {{{ Colors
 theme.fg_normal  = "#BCBCBC"
 theme.bg_normal  = "#262626"
 theme.fg_focus   = theme.bg_normal
-theme.bg_focus   = "#5F87AF"
+theme.bg_focus   = "#0af4f5"
 theme.fg_urgent  = theme.bg_normal
 theme.bg_urgent  = "#AF5F5F"
-theme.bg_systray = theme.bg_normal
+theme.bg_systray = theme.panel
 -- }}}
 
 -- {{{ Borders
 theme.useless_gap   = dpi(0)
 theme.border_width  = dpi(2)
 theme.border_normal = theme.bg_normal
-theme.border_focus  = "#5F87AF"
+theme.border_focus  = "#0af4f5"
 theme.border_marked = "#87875F"
 -- }}}
 
@@ -45,16 +47,18 @@ theme.titlebar_bg_normal = theme.bg_normal
 
 -- {{{ Taglist
 theme.taglist_fg_normal = theme.fg_normal
-theme.taglist_fg_focus = theme.bg_normal
-theme.taglist_bg_normal = theme.bg_normal
-theme.taglist_bg_focus = theme.bg_focus
+theme.taglist_fg_focus = theme.bg_focus
+theme.taglist_fg_occupied = "#058888"
+theme.taglist_bg_normal = theme.panel
+theme.taglist_bg_focus = theme.panel
+theme.taglist_bg_urgent = theme.panel
 -- }}}
 
 -- {{{ Tasklist
 theme.tasklist_fg_normal = theme.fg_normal
 theme.tasklist_fg_focus = theme.fg_normal
-theme.tasklist_bg_normal = theme.bg_normal
-theme.tasklist_bg_focus = theme.bg_normal
+theme.tasklist_bg_normal = theme.panel
+theme.tasklist_bg_focus = theme.panel
 -- }}}
 
 -- {{{ Tooltip
@@ -65,14 +69,14 @@ theme.tooltip_bg = theme.bg_normal
 -- {{{ Notification
 theme.notification_bg = theme.bg_normal
 theme.notification_fg = theme.fg_normal
-theme.notification_font = theme.font_big
-theme.notification_border_width = dpi(2)
-theme.notification_border_color = theme.bg_focus
+theme.notification_font = theme.font
+theme.notification_border_width = 0
+theme.notification_border_color = theme.notification_bg
 theme.notification_max_width = dpi(400)
 theme.notification_icon_size = dpi(64)
-theme.notification_margin = dpi(20)
+theme.notification_margin = dpi(10)
 theme.notification_shape = function(cr, width, height)
-    gears.shape.partially_rounded_rect(cr, width, height, true, false, true, true, 10)
+    gears.shape.partially_rounded_rect(cr, width, height, true, false, true, true)
 end
 -- }}}
 
@@ -118,6 +122,7 @@ theme.progressbar_fg_disabled = theme.bg_focus
 theme.graph_bg_normal = theme.bg_widget
 theme.graph_fg1 = "#5F87AF"
 theme.graph_fg2 = "#5F875F"
+theme.graph_fg3 = "#87875f"
 
 theme.systray_icon_spacing = dpi(8)
 
@@ -137,11 +142,11 @@ theme.menu_width  = dpi(100)
 -- }}}
 
 -- {{{ Icons
--- {{{ Taglist
-theme.taglist_squares_sel   = thistheme_path .. "/taglist/squarefz.png"
-theme.taglist_squares_unsel = thistheme_path .. "/taglist/squarez.png"
---theme.taglist_squares_resize = "false"
--- }}}
+
+theme.spr    = thistheme_path .. "/widgets/separators/spr.png"
+theme.sprtr  = thistheme_path .. "/widgets/separators/sprtr.png"
+theme.spr4px = thistheme_path .. "/widgets/separators/spr4px.png"
+theme.spr5px = thistheme_path .. "/widgets/separators/spr5px.png"
 
 -- {{{ Misc
 -- theme.awesome_icon           = thistheme_path .. "/archlinux-black-icon.png"
