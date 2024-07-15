0fffa722eeafaea411a88c0b45e2e861c49088eb
Nicolau Hiramoto <nicolau.hiramoto@gmail.com>
2024-07-15 11:14:24 -0300

    Alterando shell padrão para Powershell 7 no wezterm.

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -3,7 +3,7 @@ local wezterm = require 'wezterm';
 local config = wezterm.config_builder()
 
 if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
-    config.default_prog = { 'powershell.exe' }
+    config.default_prog = { 'C:\\Program Files\\PowerShell\\7\\pwsh.exe' }
 end
 
 config.initial_rows = 40
